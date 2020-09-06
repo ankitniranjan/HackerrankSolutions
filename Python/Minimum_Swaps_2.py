e0316a9bc9dd3ffbc5993e4bcc401ac032c200da
ANKIT NIRANJAN <43110505+ankitniranjan@users.noreply.github.com>
2020-09-06 13:24:21 +0530

    Update Minimum_Swaps_2.py

--- Python/Minimum_Swaps_2.py
+++ Python/Minimum_Swaps_2.py
@@ -1,3 +1,5 @@
+# Minimum swap to sort first n natural numbers
+
 #!/bin/python3
 
 import math
@@ -7,51 +9,32 @@ import re
 import sys
 
 # Complete the minimumSwaps function below.
-def minimumSwaps(arr):
-    n = len(arr) 
+def minimumSwaps(arr): 
+    swaps = 0 
+    i = 0
+    while (i < len(arr)): 
+  
+        # If current element is not at the right position 
+        if arr[i] != (i + 1): 
+  
+            while (arr[i] != i + 1): 
+                # Swap current element with correct position of that element 
+                arr[arr[i] - 1] ,arr[i]= arr[i], arr[arr[i] -1] 
+                swaps += 1
+
+        i += 1
       
-    # Create two arrays and use as pairs where first array  
-    # is element and second array is position of first element 
-    arrpos = [*enumerate(arr)] 
-      
-    # Sort the array by array element values to get right position of  
-    # every element as the elements of second array. 
-    arrpos.sort(key = lambda it:it[1]) 
-      
-    # To keep track of visited elements.  
-    # Initialize all elements as not visited or false. 
-    vis = {k:False for k in range(n)} 
-      
-    # Initialize result 
-    ans = 0
-    for i in range(n): 
-          
-        # alreadt swapped or alreadt present at correct position 
-        if vis[i] or arrpos[i][0] == i: 
-            continue
-              
-        # find number of nodes in this cycle and add it to ans 
-        cycle_size = 0
-        j = i 
-        while not vis[j]: 
-              
-            # mark node as visited 
-            vis[j] = True
-              
-            # move to next node 
-            j = arrpos[j][0] 
-            cycle_size += 1
-              
-        # update answer by adding current cycle 
-        if cycle_size > 0: 
-            ans += (cycle_size - 1) 
-    # return answer 
-    return ans 
+    return swaps 
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
     n = int(input())
+
     arr = list(map(int, input().rstrip().split()))
+
     res = minimumSwaps(arr)
+
     fptr.write(str(res) + '\n')
+
     fptr.close()
