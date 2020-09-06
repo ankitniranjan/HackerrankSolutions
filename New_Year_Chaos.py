0e1a607ac5b4a6afb177b2bbe0f11987efa2e248
ANKIT NIRANJAN <43110505+ankitniranjan@users.noreply.github.com>
2020-09-06 08:00:50 +0530

    Update New_Year_Chaos.py

--- New_Year_Chaos.py
+++ New_Year_Chaos.py
@@ -17,6 +17,32 @@ def minimumBribes(q):
             if q[j] > val:
                 moves+=1
     print(moves)
+    return
+    
+#     OR
+
+# Complete the minimumBribes function below.
+def minimumBribes(q):
+    n = 0
+    for i in range(len(q)):
+        if q[i] - 1 - i >= 3:
+            print("Too chaotic")
+            return
+    ongoing = True
+    last_index = 0
+    while ongoing:
+        for i in range(last_index, len(q)-1):
+            if q[i] > q[i+1]:
+                q[i], q[i+1] = q[i+1], q[i]
+                if i > 0:
+                    last_index = i-1
+                n += 1
+                break
+        else:
+            ongoing = False
+    print(n)
+    return
+
 if __name__ == '__main__':
     t = int(input())
 
