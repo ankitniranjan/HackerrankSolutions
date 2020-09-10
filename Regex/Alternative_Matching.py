8f7e66007ce09a29cec051cb714860db3e567998
ANKIT NIRANJAN <43110505+ankitniranjan@users.noreply.github.com>
2020-09-10 13:18:24 +0530

    Update Alternative_Matching.py

--- Regex/Alternative_Matching.py
+++ Regex/Alternative_Matching.py
@@ -4,8 +4,10 @@
 #		2. The rest of the string must contain only one or more English alphabetic letters (upper and lowercase).
 
 
-
-Regex_Pattern = r'^(Mr.)|(Mrs.)|(Dr.)|(Er.).+'	# Do not delete 'r'.
+#   contains Mr. or Mrs. or Dr. or Er. => (Mr|Mrs|Dr|Er)\.
+#   one or more alphabetical letters =>  [a-zA-Z]+
+#   rest means last of the string => $
+Regex_Pattern = r'^(Mr|Mrs|Dr|Er)\.[a-zA-Z]+$'	# Do not delete 'r'.
 
 import re
 print(str(bool(re.search(Regex_Pattern, input()))).lower())
