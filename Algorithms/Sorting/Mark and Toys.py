c7cad363ff6337c7ab3b52bf1a44a24e7e20b220
ANKIT NIRANJAN <43110505+ankitniranjan@users.noreply.github.com>
2020-09-14 16:34:28 +0530

    Create Mark and Toys.py

---
+++ Algorithms/Sorting/Mark and Toys.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the maximumToys function below.
+def maximumToys(prices, k):
+    num = 0
+    prices = sorted(prices)
+    for i in range(len(prices)):
+        if prices[i] <= k:
+            num += 1
+            k -= prices[i]
+    return num
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    prices = list(map(int, input().rstrip().split()))
+
+    result = maximumToys(prices, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
