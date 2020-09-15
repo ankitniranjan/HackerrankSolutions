a7c586a4c60a625aeab94968e90164cc8859b4a7
ANKIT NIRANJAN <43110505+ankitniranjan@users.noreply.github.com>
2020-09-15 09:37:16 +0530

    Create Compare two linked lists.py

---
+++ Data_Structure/Linked Lists/Compare two linked lists.py
@@ -0,0 +1,103 @@
+#!/bin/python3
+
+import os
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the compare_lists function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def compare_lists(llist1, llist2):
+    pass
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        llist1_count = int(input())
+
+        llist1 = SinglyLinkedList()
+
+        for _ in range(llist1_count):
+            llist1_item = int(input())
+            llist1.insert_node(llist1_item)
+            
+        llist2_count = int(input())
+
+        llist2 = SinglyLinkedList()
+
+        for _ in range(llist2_count):
+            llist2_item = int(input())
+            llist2.insert_node(llist2_item)
+
+        result = compare_lists(llist1.head, llist2.head)
+
+        fptr.write(str(int(result)) + '\n')
+
+    fptr.close()
+------------------------------------------------------------------------
+
+				# Solution
+				
+def compare_recursive(head_a, head_b):
+	if not head_a and not head_b:
+		# return True
+		return 1
+	elif (head_a and head_b) and (head_a.data == head_b.data):
+		return compare_recursive(head_a.next, head_b.next)
+	else:
+		# return False
+		return 0
+
+
+def compare_iterative(head_a, head_b):
+	node_a = head_a
+	node_b = head_b
+	while True:
+		if not node_a and not node_b:
+			# return True
+			return 1
+		elif (node_a and node_b) and (node_a.data == node_b.data):
+			node_a = node_a.next
+			node_b = node_b.next
+		else:
+			# return False
+			return 0
