bd2f6d64a4f8967f5dfec368e8e4beec8fdb4db7
ANKIT NIRANJAN <43110505+ankitniranjan@users.noreply.github.com>
2020-09-15 09:28:46 +0530

    Create Delete a Node.py

---
+++ Data_Structure/Linked Lists/Delete a Node.py
@@ -0,0 +1,96 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the deleteNode function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def deleteNode(head, position):
+	pass
+	
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist.insert_node(llist_item)
+
+    position = int(input())
+
+    llist1 = deleteNode(llist.head, position)
+
+    print_singly_linked_list(llist1, ' ', fptr)
+    fptr.write('\n')
+
+    fptr.close()
+
+-------------------------------------------------------------------------------
+						#Solution
+						
+def delete_iterative(head, position):
+	if head:
+		if position == 0:
+			head = head.next
+		else:
+			previous = None
+			current = head
+			current_position = 0
+			while (current_position < position) and (current.next is not None):
+				previous = current
+				current = current.next
+				current_position += 1
+			previous.next = current.next
+	return head
+
+
+def delete_recursive(head, position):
+	if position == 0:
+		return head.next
+	else:
+		head.next = delete_recursive(head.next, position - 1)
+		return head
