4386445ed3deb6fde38f8b53ddf2bb105743459a
ANKIT NIRANJAN <43110505+ankitniranjan@users.noreply.github.com>
2020-09-26 07:48:02 +0530

    Create Set .discard(), .remove() & .pop().py

---
+++ Python/Set .discard(), .remove() & .pop().py
@@ -0,0 +1,21 @@
+9
+1 2 3 4 5 6 7 8 9
+10
+pop
+remove 9
+discard 9
+discard 8
+remove 7
+pop 
+discard 6
+remove 5
+pop 
+discard 5
+-------------------------------------------------------
+
+n = int(input())
+s = set(map(int, input().split())) 
+for i in range(int(input())):
+    eval('s.{0}({1})'.format(*input().split()+['']))	#Appending '' in list for pop
+
+print(sum(s))
