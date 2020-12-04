72f08607eedf00968c800996e9640e9c86c6200d
ANKIT NIRANJAN <43110505+ankitniranjan@users.noreply.github.com>
2020-12-04 21:37:28 +0530

    Create Arrow Functions.py

---
+++ 10 Days of Javascript/Day-5/Arrow Functions.py
@@ -0,0 +1,21 @@
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ * 
+ * Parameter(s):
+ * nums: An array of numbers.
+ */
+
+function modifyArray(nums) {
+    nums.forEach((element, index) => {
+       nums[index] = element * (2 + element%2);
+    });
+
+    return nums;
+}
+
+function main() {
+    const n = +(readLine());
+    const a = readLine().split(' ').map(Number);
+    
+    console.log(modifyArray(a).toString().split(',').join(' '));
+}
